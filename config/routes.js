36d4de21317754138c49a575b275f5b15a8cfa24
Afreena Anjum <40909334+afreenaanjum@users.noreply.github.com>
2019-08-18 00:56:29 +0530

    Update routes.js

--- config/routes.js
+++ config/routes.js
@@ -8,23 +8,21 @@ const profileController = require('../app/controllers/profilecontroller')
 const postController = require('../app/controllers/postController')
 
 
-router.post('/register', usersController.register)
+router.post('/register', usersController.register)  //Make the routes '/users/register'. Same applies to login and account
 router.post('/login', usersController.login)
 router.get('/account', authenticateUser, usersController.show)
 router.delete('users/logout', authenticateUser, usersController.logout)
+router.post('/users/:id/add', authenticateUser, usersController.add)
+router.post('/users/:id/accept', authenticateUser, usersController.accept)
 
 
 router.post('/profile', authenticateUser, profileController.create)
 router.get('/profile', authenticateUser, profileController.list)
-router.put('/profile/:id', authenticateUser, profileController.update)
+router.put('/profile/:id', authenticateUser, profileController.update) 
 router.delete('/profile/:id', authenticateUser, profileController.destroy)
 
 router.post('/posts', authenticateUser, upload.single('post-img'), postController.create)
 router.get('/posts', authenticateUser, postController.list)
 router.delete('/posts/:id', authenticateUser, postController.destroy)
-
-
-router.post('/users/:id/add', authenticateUser, usersController.add)
-router.post('/users/:id/accept', authenticateUser, usersController.accept)
 
 module.exports = router
